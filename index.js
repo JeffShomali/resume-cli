d0cac882a5ba04cf80d59c6556003ecf51b75703
jshomali <jshomali@apple.com>
2022-11-18 16:43:11 -0800

    Update resume

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var program = require('commander');
 const chalk = require('chalk');
 var figlet = require('figlet');
 const terminalLink = require('terminal-link');
+var data =  require('./manifest')
 
 // Display Ascii
 function displayAscii() {
@@ -19,66 +20,7 @@ function displayAscii() {
 }
 
 
-//  Data
-var profile = {
-  name: "Jeff Shomali",
-  contacts: {
-    website: 'Jeffshomali.com',
-    github: 'github.com/JeffShomali ',
-    email: 'info@jeffshomali.com',
-    phone: '(415) 504-1135',
-    location: 'San Francisco, Bay Area',
-    twitter: '@jeffshomali',
-    linkedin: 'in/jeffshomali',
-    skype: 'jeffshomali'
-  },
-  objectiveAndSummary: {
-    objective: "Software Engineer | Full Stack Developer | Mobile Developer",
-    summary: "\tA recent graduated Software Engineer and Full Stack Developer with 5 years hands on experience in Front End, Back End, Web and Mobile Development.\n\tProficient in web technologies and specialize in MERN and LAMP stack, mobile development with React Native.\n\tFamiliar with most recent JavaScript frameworks and libraries.\n\tSeeking an opportunity in the following fields: ",
-    fields: [
-      'Front End Web Development',
-      'UI Development',
-      'Back End Web Development',
-      'JavaScript Development',
-      'Mobile Development (React Native)',
-      'DevOps Engineer'
-    ]
-  },
-  skills: {
-    languages: 'PHP, Node, React, React Native, Redux, JavaScript, jQuery, HTML5, CSS3, Python, Ruby, C++ and Java',
-    databases: 'SQL, MySQL, MongoDB, SQLite, Nginx, Apache, Heroku, Digital Ocean',
-    frameworks: 'Meteor, Express, Ruby on Rails, Django, Laravel, Bootstrap, SemanticUI',
-    cms: 'WordPress, Magento, WooCommerce, Drupal',
-    others: 'NPM, Yarn, Yeoman, Webpack, Git & GitHub, Babel',
-    editors: 'Sublime, Atom, Visual Studio Code, PHPStorm, WebStorm'
-  },
-  experience: {
-    1: 'Senior Software Engineer \t\t\t\t\t\t\t\t\t Nortek Security Control - 2018 - Present',
-    2: 'Web Analytics Implementation Specialist \t\t\t\t\t\t\t Robert Half Technology 2018 - 2018 ',
-    3: 'Full Stack Web Developer \t\t\t\t\t\t\t\t\t TUTYO 2016 – 2018',
-    4: 'Full Stack Web Developer \t\t\t\t\t\t\t\t\t Purrivacy (Intern) 2015– 2016',
-    5: 'Freelance Web Developer  \t\t\t\t\t\t\t\t\t 2013 – Forever'
-  },
-  projects: {
-    1: `iOS Cryptocurrency App: iOS Cryptocurrency app to watch all coin’s market data and news. Built in React NaJve, Restful API.`,
-    2: 'San Francisco Bart Application: iOS application for Bay Area Rapid Transit (BART), built in React NaJve and Flux router.',
-    3: 'Lifo: It Is a Cryptocurrency website for collection all news about the blockchain and crypto currency.',
-    4: 'Visual Studio Code Snippet Generator: Web application to generate snippet code for VSCode. Built in React and NodeJS.'
-  },
-  education: {
-    1: 'Bachelor of Science in Computer Science. Trident University \t- Honor Student, GPA 3.6, Cypress, CA, April 2018',
-    2: 'Associate of Science in Computer Science. Diablo Valley College \t- Honor Student, GPA 3.8, Pleasant Hill, CA, May 2016',
-    3: 'Certified Advanced Java Programming, Program Design and Computer Architecture \t- Diablo Valley College 2016.',
-    4: 'Certified Front-End Web Developer Nanodegree, \t- Udacity, June 2016'
-  },
-  others: {
-    1: 'US Citizen',
-    2: 'Twitter: @jeffshomali',
-    3: 'Skype: jeffshomali',
-    4: 'Schedule a call: calendly.com/jeffshomali'
-  }
-}
-
+const profile = data.profile
 
 
 function resume() {
@@ -150,7 +92,7 @@ function others() {
 program
   .version('0.0.1', '-v, --version')
   .option('-a, --all', 'Display All Sections')
-  .option('-o, --objective', 'Dsiplay Objective and Summary')
+  .option('-o, --objective', 'Display Objective and Summary')
   .option('-s, --skills', 'Display Skills')
   .option('-e, --experience', 'Display Experience')
   .option('-p, --projects', 'Display Projects')
---
+++ manifest.js
@@ -0,0 +1,63 @@
+//  Data
+const profile = {
+  name: "Jeff Shomali",
+  contacts: {
+    website: 'https//:shoma.li',
+    github: 'github.com/JeffShomali',
+    email: 'jeff@shoma.li',
+    phone: '(415) 504-1135',
+    location: 'San Francisco, Bay Area',
+    twitter: '@jeffshomali',
+    linkedin: 'in/jeffshomali',
+    skype: 'jeffshomali'
+  },
+  objectiveAndSummary: {
+    objective: "Software Engineer | Full Stack Developer | Mobile Developer",
+    summary: "\tInventive Full Stack Engineer with 5+ years of broad experience in Front-end, Back-end, and Mobile Development\n\tareas with a willingness to learn and master Web or Mobile Development with NodeJS, ReactJS, React Native, Redux, NextJS. Experienced with \n\tthe most popular MVC (Express, Laravel, Adonis, Ruby on Rails, and Django).",
+
+    fields: [
+      'Front End Web Development',
+      'UI Development',
+      'Back End Web Development',
+      'JavaScript Development',
+      'Mobile Development (React Native)',
+      'DevOps Engineer'
+    ]
+  },
+  skills: {
+    "Front End": "\n\t\tHTML/CSS, Sass/LESS, DOM, Bootstrap, Semantic UI, Bulma, Tailwind. Javascript, jQuery, ES6/ES2015, React.js, Redux, Redux-Saga, Next.js, Vue.js. RESTful API, GraphQL, Babel, NPM, Webpack. Meteor, Cypress, Puppeteer, Electron.",
+    "Back End": "\n\t\tMERN (MongoDB, Express, React, Node), Next. LAMP (Linux, Apache, MySQL, PHP), MVC (Laravel, Ruby on Rails, Django/Flask, Adonis) RabbitMQ, ActiveMQ, Redis, Microservices",
+    "Mobile (iOS/Android)": '\n\t\tJavascript, React, React Native, Swift, Redux, Redux-Saga, Cordova.Firebase, Expo, Code Push, Jest, Mocha.',
+    Database: '\n\t\tMySQL, MariaDB, PostgreSQL, SQLLite, Relational Databases, ORM. NoSQL, Cassandra, MongoDB.',
+    DevOps: '\n\t\tLinux/Unix, Git, Apache, Nginx. AWS(EC2, IAM, S3, Route53, Code Commit, Code Deploy, Beanstalk, Lambda), VPS, Jenkins, Redis, Heroku, Docker, Vagrant, Azure Active Directory, SAML, LDAP.',
+    Others: '\n\t\tOOP, SOLID principles, Program Design and Data Structure. Agile, Jira, Confluence, API Design, Unit/E2E testing.'
+  },
+  experience: {
+    1: "Senior Software Engineer \t\t\t\t\t\t\t\t\t Apple - 2021 - Present",
+    2: 'Full Stack Engineer      \t\t\t\t\t\t\t\t\t Nortek Security Control - 2018 - 2021',
+    3: 'Web Developer            \t\t\t\t\t\t\t\t\t Robert Half Technology 2018 - 2018 ',
+    4: 'Full Stack Developer & Founder \t\t\t\t\t\t\t\t\t TUTYO 2016 – 2016 - 2018',
+    5: 'Full Stack Developer (Intern) \t\t\t\t\t\t\t\t\t Purrivacy  2015– 2016',
+  },
+  projects: {
+    1: `iOS Cryptocurrency App: iOS Cryptocurrency app to watch all coin’s market data and news. Built in React NaJve, Restful API.`,
+    2: 'San Francisco Bart Application: iOS application for Bay Area Rapid Transit (BART), built in React NaJve and Flux router.',
+    3: 'Lifo: It Is a Cryptocurrency website for collection all news about the blockchain and crypto currency.',
+    4: 'Visual Studio Code Snippet Generator: Web application to generate snippet code for VSCode. Built in React and NodeJS.'
+  },
+  education: {
+    1: "Masters of Science in Technology Management (MSITM)                           \t- American intercontinental University - Class of Feb 2022",
+    2: 'Bachelor of Science in Computer Science. Trident University                   \t- Honor Student, GPA 3.6, Cypress, CA, April 2018',
+    3: 'Associate of Science in Computer Science. Diablo Valley College               \t- Honor Student, GPA 3.8, Pleasant Hill, CA, May 2016',
+    4: 'Certified Advanced Java Programming, Program Design and Computer Architecture \t- Diablo Valley College 2016.',
+    5: 'Certified Front-End Web Developer Nanodegree                                  \t- Udacity, June 2016'
+  },
+  others: {
+    1: 'US Citizen',
+    2: 'Twitter: @jeffshomali',
+    3: 'Skype: jeffshomali',
+    4: 'Schedule a call: calendly.com/jeffshomali'
+  }
+}
+
+exports.profile = profile;
